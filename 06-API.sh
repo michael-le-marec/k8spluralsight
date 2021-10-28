ba36446c789fee097b8f4eed2c7e873177d1412e
Michaël Le Marec <mlemarec@symtrax.eu>
2021-10-28 13:23:00 +0200

    Update

--- 06-API.sh
+++ 06-API.sh
@@ -100,3 +100,131 @@ kubectl delete pod hello-world
 
 kubectl get all -v 6
 
+# MANAGING OBJECTS WITH LABELS, ANNOTATIONS AND NAMESPACES
+
+kubectl get namespaces
+
+kubectl api-resources --namespaced=true | head
+kubectl api-resources --namespaced=false | head
+
+kubectl describe namespaces
+
+kubectl describe namespaces kube-system
+
+kubectl get pods --all-namespaces
+
+kubectl get all --all-namespaces
+
+kubectl get pods --namespace kube-system
+
+kubectl create namespace playground1
+
+kubectl create namespace Playground1 # only low case
+
+kubectl apply -f deployment-namespace.yaml
+
+kubectl run hello-world-pod \
+    --image=gcr.io/google-samples/hello-app:1.0 \
+    --generator=run-pod/v1 \
+    --namespace playground1
+
+kubectl get pods --namespace playground1
+kubectl get pods -n playground1
+
+kubectl get all -n playground1
+
+# Operations on resources in a namespace
+
+kubectl delete pods --all -n Playground1
+
+kubectl delete namespaces playground1
+kubectl delete namespaces playgroundinyaml --wait=false
+
+kubectl get all --all-namespaces
+
+# Working with Labels
+
+kubectl apply -f podswithlabels.yaml
+
+kubectl get pods --show-labels
+
+kubectl describe pod nginx-pod-1 | head
+
+kubectl get pods --selector tier=prod
+kubectl get pods --selector tier=qa
+kubectl get pods -l tier=prod
+kubectl get pods -l tier=prod --show-labels
+
+kubectl get pods -l 'tier=prod,app=MyWebApp' --show-labels
+kubectl get pods -l 'tier=prod,app!=MyWebApp' --show-labels
+kubectl get pods -l 'tier in (prod,qa)'
+kubectl get pods -l 'tier notin (prod,qa)'
+
+kubectl get pods -L tier # Add labels as column
+kubectl get pods -L tier,app
+
+kubectl label pod nginx-pod-1 tier=non-prod --overwrite # Change labels of a pod
+kubectl get pod nginx-pod-1 --show-labels
+
+kubectl label pod nginx-pod-1 another=Label
+kubectl get pod nginx-pod-1 --show-labels
+
+kubectl label pod nginx-pod-1 another- # remove label
+kubectl get pod nginx-pod-1 --show-labels
+
+kubectl label pod --all tier=non-prod --overwrite
+kubectl get pod --show-labels
+
+kubectl delete pod -l tier=non-prod --wait=false
+kubectl get pod --show-labels
+
+# LABELS FOR DEPLOYMENTS AND REPLICASETS
+
+kubectl apply -f deployment-label.yaml
+
+kubectl apply -f service.yaml
+
+kubectl describe deployment hello-world
+
+kubectl describe replicaset hello-world
+
+kubectl get pods --show-labels
+
+kubectl label pod hello-world-54575d5b77-7rk7d pod-template-hash=DEBUG --overwrite # Technique in order to debug a running pod
+
+kubectl get pods --show-labels
+
+kubectl get service
+
+kubectl describe service hello-world
+
+kubectl describe endpoints hello-world
+
+kubectl get pod -o wide --show-labels
+
+kubectl label pod hello-world-54575d5b77-7rk7d app=DEBUG --overwrite
+
+kubectl get pod -o wide --show-labels
+kubectl describe endpoints hello-world
+
+kubectl delete deploy hello-world --wait=false
+kubectl delete service hello-world --wait=false
+kubectl delete pod hello-world-54575d5b77-7rk7d
+
+kubectl get nodes --show-labels
+
+kubectl label node c1-node2 disk=local_ssd
+kubectl label node c1-node3 hardware=local_gpu
+
+kubectl get node -L disk,hardware
+
+kubectl apply -f podtonodes.yaml
+
+kubectl get node -L disk,hardware
+kubectl get pods -o wide
+
+kubectl label node c1-node2 disk-
+kubectl label node c1-node3 hardware-
+kubectl delete pod nginx-pod --wait=false
+kubectl delete pod nginx-pod-gpu --wait=false
+kubectl delete pod nginx-pod-ssd --wait=false
